8696d02e53deddf491d16ec18f35150dd9c7b8d9
manojkumarsaini19 <162662119+manojkumarsaini19@users.noreply.github.com>
2024-03-08 09:57:18 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,37 +1,38 @@
-let currentInput ='0';
-const displayElement = document. getElementById('display');
+let currentInput = 0;
+const displayElement = document.getElementById('display');
 function handleButtonClick(event){
-    const buttonVelue =event.target.innertext;
-    if(buttonvalue==='=' || buttonValue==='C'){
+    const buttonValue = event.target.innerText;
+    console.log(buttonValue);
+    if(buttonValue==='=' || buttonValue==='C'){
         calculate();
 } else if (buttonValue ==='C'){
     clearDisplay();
 }else{
-    appendTodisplay(buttonvalue);
+    appendToDisplay(buttonValue);
 }
 }
   
 function appendToDisplay(value){
-    if( currentInput==='0'){
+    if( currentInput===0){
         currentInput=value;
     }else{
         currentInput+= value;
     }
     updateDisplay();
     }
-    function updateDisplaydate(){
-        displayElement.innerText=currentInput
+    function updateDisplay(){
+        displayElement.innerText=currentInput;
     } 
     function clearDisplay(){
         currentInput='0';
     }
     function calculate(){
         try{
-            currentInput=eval(currebtInput).toString();
+            currentInput=eval(currentInput).toString();
             updateDisplay()
         }catch (error){
             currentInput='Error';
-            updateDisplaydate();
+            updateDisplay();
         }
         }
     
